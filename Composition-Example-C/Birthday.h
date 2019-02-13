94204afbd14fc8331d1af192bac58bd7822a63d9
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-12 20:02:04 -0700

    Create Birthday.h

---
+++ Composition-Example-C/Birthday.h
@@ -0,0 +1,21 @@
+#ifndef BIRTHDAY_H
+#define BIRTHDAY_H
+
+#include <string>
+
+
+class Birthday
+{
+public:
+	Birthday(int m, int d, int y);
+	void printDate();
+private:
+	int month;
+	int day;
+	int year;
+
+
+
+};
+
+#endif
