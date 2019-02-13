b1faedbe8b0a75bf09ed75d23fc2023fea3587c7
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-12 19:58:59 -0700

    Create Sally.h

---
+++ Operator-Overloading/Sally.h
@@ -0,0 +1,16 @@
+#ifndef SALLY_H
+#define SALLY_H
+
+
+class Sally
+{
+    public:
+        int num;
+        Sally();
+        Sally(int);
+        Sally operator+(Sally); // take two number properties and them together
+
+
+};
+
+#endif // SALLY_H
